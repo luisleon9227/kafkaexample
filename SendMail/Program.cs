0ab4319347c8402020e5c114c0f13f066eafaacf
Luis Alfredo Leon Britto <luis.leon+Siigo@siigo.com>
2024-01-20 12:13:16 -0500

    Kafka example

---
+++ SendMail/Program.cs
@@ -0,0 +1,37 @@
+﻿using Confluent.Kafka;
+
+var config = new ConsumerConfig
+{
+    BootstrapServers = "localhost:9092",
+    GroupId = "mail-consumer-group",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+using var consumer = new ConsumerBuilder<Ignore, string>(config).Build();
+
+consumer.Subscribe("test");
+
+CancellationTokenSource cancellationToken = new();
+
+try
+{
+    while (true)
+    {
+        var consumeResult = consumer.Consume(cancellationToken.Token);
+        if (consumeResult.Message != null)
+        {
+            var correo = consumeResult.Message.Value;
+            Console.WriteLine($"Mensaje recibido: {correo}");
+        }
+
+    }
+
+}
+catch (OperationCanceledException)
+{
+    consumer.Close();
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+}
